454d0302fbc33cca663a568085dad6b8db036583
Codehexa <codehexa@naver.com>
2021-09-07 16:05:21 +0900

    2021.09.07.37 - Send SmsSendResults

--- yepProject/app/Http/Controllers/SmsViewController.php
+++ yepProject/app/Http/Controllers/SmsViewController.php
@@ -47,8 +47,6 @@ class SmsViewController extends Controller
             $clId = $smsPaper->cl_id;
         }
 
-        dd($smsPapers);
-
         $student = Students::where('class_id','=',$clId)->where('parent_hp','=',$upTel)->first();
 
         $opinionsAll = [];
