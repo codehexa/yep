f1e3c9039549375f0b9348ef5f934619f18ba8fd
Codehexa <codehexa@naver.com>
2021-09-08 10:22:57 +0900

    2021.09.08.04 - bms

--- yepProject/database/seeders/DatabaseSeeder.php
+++ yepProject/database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             BmsStudyTimesSeeder::class,
             BmsCurriculumsSeeder::class,
             SmsPageSettingSeeder::class,
+            BmsWorkbookSeeder::class,
         ]);
     }
 }
