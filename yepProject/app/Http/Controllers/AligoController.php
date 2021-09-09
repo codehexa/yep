2ed5aec7743a9dd9083706c2d3a14ad177192906
Codehexa <codehexa@naver.com>
2021-09-09 10:39:49 +0900

    2021.09.09.12 - bms

--- yepProject/app/Http/Controllers/AligoController.php
+++ yepProject/app/Http/Controllers/AligoController.php
@@ -93,10 +93,6 @@ class AligoController extends Controller
             $sms['msg_type']    = Configurations::$ALIGO_MSG_TYPE;
 
             $msgRoot = $lmsDatum->bsl_send_text;
-            $msgTitleRoot = explode("/\r\n/",$msgRoot);
-            $msgTitle = $msgTitleRoot[0];
-
-            $sms['title']   = $msgTitle;
 
             $tels = $lmsDatum->tels;
             $telsArray = explode(",",$tels);
