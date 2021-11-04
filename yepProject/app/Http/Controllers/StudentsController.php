a1768966a0a74bbc8511bfba4523fda44445cd3d
Codehexa <codehexa@naver.com>
2021-11-04 10:52:43 +0900

    2021.11.04.15 - Student Upload

--- yepProject/app/Http/Controllers/StudentsController.php
+++ yepProject/app/Http/Controllers/StudentsController.php
@@ -181,6 +181,7 @@ class StudentsController extends Controller
                 $absCode = $vals[14];
 
                 // class name
+                dd($clRoot);
                 $clRoot1 = substr($clRoot,0,strpos($clRoot,Configurations::$EXCEL_CLASS_RIP_CODE)); // excel 반명 추출. 학원코드 + 반코드
                 $clAcCode = substr($clRoot1,0,1);
                 $clCode = substr($clRoot1,1,strlen($clRoot1));
