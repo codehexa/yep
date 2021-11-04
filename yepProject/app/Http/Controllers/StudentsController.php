8c7af85a34014091df5ec145860a00de2e1893fb
Codehexa <codehexa@naver.com>
2021-11-04 10:50:36 +0900

    2021.11.04.12 - Student Upload

--- yepProject/app/Http/Controllers/StudentsController.php
+++ yepProject/app/Http/Controllers/StudentsController.php
@@ -194,7 +194,7 @@ class StudentsController extends Controller
                     }
                 }
 
-                dd ($nowAcId);
+                dd ($acHash . $clAcCode);
 
 
                 // 선생님 찾기
