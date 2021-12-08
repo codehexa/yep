115dd23c45ad9d0a56b546eac6ace849b8336783
Codehexa <codehexa@naver.com>
2021-12-08 16:13:21 +0900

    2021.12.08.16

--- yepProject/app/Http/Controllers/StudentsController.php
+++ yepProject/app/Http/Controllers/StudentsController.php
@@ -226,7 +226,9 @@ class StudentsController extends Controller
                 if ($teacher == ""){
                     return redirect()->back()->withErrors(['msg'=>'NO_TEACHER_DATA']);
                 }
-                $absCode = number_format($vals[15],"","");   // 학번
+                $absCode = preg_replace('/(0)(\d)/','$2', $vals[15]);
+
+                //$absCode = number_format($vals[15],"","");   // 학번
 
                 //dd($gradeHashes);
                 // school grade 찾기
