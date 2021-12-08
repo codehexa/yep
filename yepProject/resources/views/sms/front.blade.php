22b218c30d7d1a82ba5ae39d52c08eebfd38758c
Codehexa <codehexa@naver.com>
2021-12-08 20:52:03 +0900

    2021.12.08.74 - 시험 폼 매칭 패치

--- yepProject/resources/views/sms/front.blade.php
+++ yepProject/resources/views/sms/front.blade.php
@@ -430,11 +430,11 @@
                 type:"POST",
                 dataType:"json",
                 success:function(msg){
-                    $("#preview_panel").load("/sms/views",[
-                        {"_token":$("#input[name='_token']").val()},
-                        {"up_code":msg.spcode},
-                        {"up_parent_tel":msg.data.parent_hp}
-                    ]);
+                    $("#preview_panel").load("/sms/views",
+                        {"_token":$("#input[name='_token']").val(),
+                        "up_code":msg.spcode,
+                        "up_parent_tel":msg.data.parent_hp}
+                    );
                 }
             });
         });
