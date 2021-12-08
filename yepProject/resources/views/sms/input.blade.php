2f0480313a834fe8bdb0916345e65066c4d41607
Codehexa <codehexa@naver.com>
2021-12-08 20:06:49 +0900

    2021.12.08.67 - 시험 폼 매칭 패치

--- yepProject/resources/views/sms/input.blade.php
+++ yepProject/resources/views/sms/input.blade.php
@@ -121,7 +121,7 @@
                         @foreach ($tItems as $item)
                             @if ($item->sj_has_child == "N")
                                 @php ($field_name = "score_".$num)
-                                <td class="form-group ">
+                                <td class="form-group d-flex justify-content-center">
                                         <input type="text"
                                                name="score_{{ $num }}[]" id="f_{{ $num }}_{{ $i }}_{{ $data[$i]["id"] }}" value="{{ $data[$i][$field_name] }}"
                                                max="{{ $item->sj_max_score }}" min="0"
