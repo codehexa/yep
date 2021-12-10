f9c99c329627bc2c2c9425e0be98a1d6b4c4ec58
Codehexa <codehexa@naver.com>
2021-12-10 14:19:04 +0900

    2021.12.10.11 - 엑셀버그

--- yepProject/resources/views/sms/input.blade.php
+++ yepProject/resources/views/sms/input.blade.php
@@ -288,6 +288,7 @@
 
         $(document).on("click",".fn_chbox",function(){
             let rid = $(this).data("rid");
+            console.log(rid);
             if ($(this).prop("checked")){
                 $("#" + rid).val("Y");
             }else{
