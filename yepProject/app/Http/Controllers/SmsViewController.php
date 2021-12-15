4d25cb45a02328284d94fa01a1ca502a49613a01
Codehexa <codehexa@naver.com>
2021-12-15 15:48:37 +0900

    2021.12.15.37 - SmsViewController.php

--- yepProject/app/Http/Controllers/SmsViewController.php
+++ yepProject/app/Http/Controllers/SmsViewController.php
@@ -250,7 +250,7 @@ class SmsViewController extends Controller
     public function viewDetailPreview($pid){
         $upCode = $pid;
 
-        $smsPapers = SmsPapers::where('sp_code','=',$upCode)->get();
+        $smsPapers = SmsPapers::where('sp_code','=',$upCode)->first();
 
         if (!isset($smsPapers)){
             die("NO Data");
