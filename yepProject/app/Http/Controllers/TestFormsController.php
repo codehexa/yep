667e30c223676c67e27f041419732e570eae3c7d
Codehexa <codehexa@naver.com>
2021-12-15 10:46:13 +0900

    2021.12.15.03 - TestFormsController.php

--- yepProject/app/Http/Controllers/TestFormsController.php
+++ yepProject/app/Http/Controllers/TestFormsController.php
@@ -36,7 +36,7 @@ class TestFormsController extends Controller
         if ($gradeId != "") $wheres[] = ["grade_id","=",$gradeId];
 
         if (sizeof($wheres) > 0){
-            $data = TestForms::where('grade_id','=',$gradeId)->orderBy('form_title','asc')->paginate($limit);
+            $data = TestForms::where('grade_id','=',$gradeId)->orderBy('form_title','asc')->toSql();//paginate($limit);
             dd($data);
         }else{
             $data = TestForms::orderBy('form_title','asc')->paginate($limit);
