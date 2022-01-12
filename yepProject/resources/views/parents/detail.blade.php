1a107895ec1d61fdc7f4c0cfc3008fa2ef3c268f
Codehexa <codehexa@naver.com>
2022-01-12 10:38:37 +0900

    2022. 01. 12. 10 - chart.js

--- yepProject/resources/views/parents/detail.blade.php
+++ yepProject/resources/views/parents/detail.blade.php
@@ -209,6 +209,7 @@
                                 xAxes: [{
                                     stacked: true,
                                     ticks: {
+                                        display: true,
                                         callback: function(value,index,values){
                                             return value;
                                         }
