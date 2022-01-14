16973d21b9fc63872a16f9bf9b30dd83aea3736a
Codehexa <codehexa@naver.com>
2022-01-14 11:30:44 +0900

    2022. 01. 14. 21 - SmsViewController.php

--- yepProject/app/Http/Controllers/SmsViewController.php
+++ yepProject/app/Http/Controllers/SmsViewController.php
@@ -40,8 +40,6 @@ class SmsViewController extends Controller
         $upTel = $request->get("up_parent_tel");
         $studentId = $request->get("up_student_id");
 
-        dd($request);
-
         $smsPapers = SmsPapers::where('sp_code','=',$upCode)->first();
         //$smsPapers = $smsPapersRoot->first();
 
