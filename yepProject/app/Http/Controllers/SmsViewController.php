ff5c5f080fbdb71d92927285f77501bd87a8b860
Codehexa <codehexa@naver.com>
2022-01-24 15:09:07 +0900

    2022. 01. 24.01 - SmsViewController.php

--- yepProject/app/Http/Controllers/SmsViewController.php
+++ yepProject/app/Http/Controllers/SmsViewController.php
@@ -490,7 +490,11 @@ class SmsViewController extends Controller
                             $reOpinions[] = ["bTitle"=>$opinionsAll[$ar]["title"],"child"=>[["sTitle"=>$opinionsAll[$ar]['sub_title'],"txt"=>$opinionsAll[$ar]['txt']]]];
                         }
                     } else {
-                        $reOpinions[] = ["bTitle"=>$opinionsAll[$ar]["title"],"child"=>[["sTitle"=>$opinionsAll[$ar]['sub_title'],"txt"=>$opinionsAll[$ar]['txt']]]];
+                        if (isset($opinionsAll[$ar]['sub_title'])){
+                            $reOpinions[] = ["bTitle"=>$opinionsAll[$ar]["title"],"child"=>[["sTitle"=>$opinionsAll[$ar]['sub_title'],"txt"=>$opinionsAll[$ar]['txt']]]];
+                        }else{
+                            $reOpinions[] = ["bTitle"=>$opinionsAll[$ar]["title"],"child"=>[["sTitle"=>"","txt"=>$opinionsAll[$ar]['txt']]]];
+                        }
                     }
                 }
             }
